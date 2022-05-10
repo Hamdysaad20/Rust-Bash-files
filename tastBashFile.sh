f67543bf2ef4430be29ce8715472405ee3ea9e1d
Hamdy Saad <73494683+Hamdysaad20@users.noreply.github.com>
2022-05-10 11:14:45 +0200

    Create tastBashFile.sh

---
+++ tastBashFile.sh
@@ -0,0 +1,6 @@
+# tasks
+• Create a directory with name Direc1.
+• Create a bash script inside Direc1 Folder does that.
+   o Read two values from the user.
+   o Find out the odd and even number between these two values.
+   o Run this bash script
