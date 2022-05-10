defc1daa8ac609f3d8c9bb65d8522792e1b88d0b
Hamdy Saad <73494683+Hamdysaad20@users.noreply.github.com>
2022-05-10 12:41:05 +0200

    Update tastBashFile.sh

--- tastBashFile.sh
+++ tastBashFile.sh
@@ -1,6 +1,31 @@
 # tasks
-• Create a directory with name Direc1.
-• Create a bash script inside Direc1 Folder does that.
-   o Read two values from the user.
+• Create a directory with name Direc1. D
+• Create a bash script inside Direc1 Folder does that.D
+   o Read two values from the user.D
    o Find out the odd and even number between these two values.
    o Run this bash script
+   
+   mkdir Direc1
+   cd Direc1
+   touch bashFile.sh
+   chmod +x bashFile.sh
+   clear 
+   vi bashFile.sh
+   
+   read -p "Enter the first Number : " num1
+   read -p "Enter the sec Number : " num2
+
+if [ `expr $num1 % 2` == 0 ]
+then
+	echo "$num1 is even"
+else
+	echo "$num1 is Odd"
+fi
+
+if [ `expr $num2 % 2` == 0 ]
+then
+	echo "$num1 is even"
+else
+	echo "$num1 is Odd"
+fi
+   
