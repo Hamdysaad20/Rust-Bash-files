0d2ce5028861ba9065f8467f30a8274727292a2e
Hamdy Saad <73494683+Hamdysaad20@users.noreply.github.com>
2022-05-15 18:06:40 +0200

    Update manual_mut.rs

--- Rust/manual_mut.rs
+++ Rust/manual_mut.rs
@@ -3,3 +3,5 @@
     let var_1=5;
     println!("hi ,{}",var_1)
 // when you assign new value just put "let" 
+// if the var is mut, you can change it's value not it's type
+// to change it's type you need to put "let", so it's a new hole var .
