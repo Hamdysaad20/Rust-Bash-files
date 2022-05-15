37d890094bf20d412af11f91d36e130cf0d6361d
Hamdy Saad <73494683+Hamdysaad20@users.noreply.github.com>
2022-05-15 14:54:29 +0200

    Get the numbers of tries .

--- Rust/guss_the_number.rs
+++ Rust/guss_the_number.rs
@@ -3,10 +3,13 @@ use std::io;
 use rand::Rng;
 
 //create gussing number game
+//get the number of tries
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut tries_number:i32 =0;
     loop {
+
         println!("Please input your guess.");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess)
@@ -16,11 +19,14 @@ fn main() {
             Err(_) => continue,
         };
         println!("You guessed: {}", guess);
+        tries_number = tries_number + 1;
+
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+
+            Ordering::Less => println!("Too small! \n number of tries: {}", tries_number),
+            Ordering::Greater => println!("Too big! \n number of tries: {}", tries_number),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win! \n number of tries: {}", tries_number);
                 break;
             }
         }
