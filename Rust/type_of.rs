ddb3ceadd833172d367e732c1e8e141a4907b849
Hamdy Saad <73494683+Hamdysaad20@users.noreply.github.com>
2022-05-15 10:24:20 +0200

    Create type_of.rs

---
+++ Rust/type_of.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut s = "Hello";
+    let mut i = 42;
+
+    print_type_of(&s); // &str
+    print_type_of(&i); // i32
+    print_type_of(&main); // playground::main
+    print_type_of(&print_type_of::<i32>); // playground::print_type_of<i32>
+    print_type_of(&{ || "Hi!" }); // playground::main::{{closure}}
+}
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
