deb877da2641d5e70135e37dc38e614252d1a659
Hamdy Saad <73494683+Hamdysaad20@users.noreply.github.com>
2022-05-15 18:01:19 +0200

    Create scope_is_important.rs

---
+++ Rust/scope_is_important.rs
@@ -0,0 +1,11 @@
+fn main() {
+   let  var_1=4;
+    println!("hi, {}",var_1);
+    let var_1=var_1+2;
+    println!("hi ,{}",var_1);
+    {
+        let var_1=12;
+        println!("lol ,{}",var_1)
+    }
+}
+// the scope is important .
